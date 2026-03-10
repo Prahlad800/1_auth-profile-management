c051c238aa3ce854fac791774f6e3ef38930e0ba
prahlad800 <prahlad8009872468@gmail.com>
2026-03-11 01:21:48 +0530

    update server file logic

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,9 @@
 import express from "express"
 import dotenv from "dotenv"
 import app from "./app.js"
-import { connectDB } from "./db/db.js"
+
 dotenv.config()
 const port = process.env.PORT
-
-connectDB()
 
 app.listen(port, ()=>{
    console.log(`Server running on port ${port}`)
