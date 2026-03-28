839128188ecb3a14059533ef5f6f3e9a2b26d166
prahlad800 <prahlad8009872468@gmail.com>
2026-03-29 00:43:47 +0530

    compleact frondend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,29 @@ import './App.css'
 import Home from './page/Home'
 import Login from './page/Login'
 import Signup from './page/Signup'
+import { useState } from 'react'
+import RefreshHandler from './utils/RefreshHandler'
 
 
 
 function App() {
- 
+  const [isAuth,setIsAuth]=useState(false)
+
+ const PriveteRoute =({element})=>{
+  return isAuth ? element : <Navigate to="/login"/>
+ }
+
 
   return (
+    <div>
+      <RefreshHandler setIsAuth={setIsAuth}/>
     <Routes>
       <Route path="/"element={<Navigate to="/login"/>}/>
-      <Route path="/home" element={<Home />} />
+      <Route path="/home" element={<PriveteRoute   element={<Home />}/> } />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
+    </div>
   )
 }
 
